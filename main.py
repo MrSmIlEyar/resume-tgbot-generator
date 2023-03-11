ceac80ce6962ccd74b80e26be8a2778110f7dadc
Fenikonir <belovemil@gmail.com>
2023-03-11 20:36:47 +0300

    Добавил функцию генерации пустого файла

--- main.py
+++ main.py
@@ -26,14 +26,13 @@ async def gendoc_command_handler(message: types.Message):
     # запрашиваем у пользователя название файла
     user_id = message.from_user.id
 
-    # создаем пустой документ
-    document = Document()
-
+    user = message.from_user
+    user_info = get_user_info(user)
     # сохраняем документ с заданным именем
     filename = f"{user_id}.docx"
     directory = "Documents/"  # замените на свой путь
     filepath = os.path.join(directory, filename)
-    document.save(filepath)
+    generate_docx_with_array_elements(user_info, filepath)
 
     # отвечаем пользователю сообщением об успешном создании файла
     await message.answer(f"Файл {filename} успешно создан.")
@@ -61,6 +60,34 @@ async def send_doc(message: types.Message):
 async def echo(message: types.Message):
     # Отправляем эхо-сообщение
     await message.answer(f"Ты написал: {message.text}")
+
+
+def generate_docx_with_array_elements(array, direct):
+    # Создаем объект документа
+    document = Document()
+
+    # Добавляем заголовок документа
+    document.add_heading('Information about User', level=0)
+
+    # Добавляем элементы массива на новые строки
+    for item in array:
+        paragraph = document.add_paragraph(str(item))
+        paragraph.style = document.styles['List Bullet']
+
+    # Сохраняем документ в файл
+    document.save(direct)
+
+
+def get_user_info(user: types.User):
+    user_info = [
+        f"User ID: {user.id}",
+        f"Username: {user.username}",
+        f"First name: {user.first_name}",
+        f"Last name: {user.last_name}",
+        f"Language: {user.language_code}",
+        f"Is bot: {user.is_bot}"
+    ]
+    return user_info
 
 
 # Запускаем бота
